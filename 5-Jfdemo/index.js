46d96f077604e270b1beaac8b6d0627cfa3390d2
jf981111 <983446103u@qq.com>
2019-04-28 15:21:44 +0800

    setState异步回调

---
+++ 5-Jfdemo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// 引入样式
+import './styles/index.css'
+// 引入组件
+import Jfdemo from './component/Jfdemo'
+
+ReactDOM.render(
+    <Jfdemo></Jfdemo>,
+    document.getElementById('root')
+)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
+// setState 是异步的
 import React from 'react'
 import ReactDOM from 'react-dom'
-// 引入样式
-import './styles/index.css'
-// 引入组件
-import Jfdemo from './component/Jfdemo'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 1
+        }
+
+        // this.fn = this.fn.bind(this)
+    }
+
+    render() {
+        return (
+            <div>
+                { this.state.count }
+                <button onClick={ this.fn.bind(this) }>加10</button>
+            </div>
+        )
+    }
+
+    fn() {
+        this.setState({
+            count: this.state.count + 1
+        }, () => {
+            // 异步回调，当数据修改完成之后触发
+            console.log(this.state.count)
+        })
+
+        
+    }
+}
 
 ReactDOM.render(
-    <Jfdemo></Jfdemo>,
+    <App />,
     document.getElementById('root')
 )
